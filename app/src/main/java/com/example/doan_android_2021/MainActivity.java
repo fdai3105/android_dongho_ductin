a79aad79e00f02f2de2543565e30714bad732501
FiDai <fdai3105@gmail.com>
2020-12-18 10:31:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/doan_android_2021/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.doan_android_2021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
